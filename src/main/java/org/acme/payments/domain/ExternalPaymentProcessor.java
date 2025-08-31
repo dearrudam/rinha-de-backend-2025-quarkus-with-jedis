9e61ee118197060478023e07b55f955618a7bdd8
Maximillian Arruda <dearrudam@gmail.com>
2025-08-30 23:18:12 -0300

    fix: injetar dependências via construtor
    Refatoração da classe ExternalPaymentProcessor para injetar
    HttpClient e ExternalPaymentLoadBalancer via construtor,
    removendo a necessidade de um construtor padrão e melhorando
    a injeção de dependências.

--- src/main/java/org/acme/payments/domain/ExternalPaymentProcessor.java
+++ src/main/java/org/acme/payments/domain/ExternalPaymentProcessor.java
@@ -1,34 +1,21 @@
 package org.acme.payments.domain;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 @ApplicationScoped
 public class ExternalPaymentProcessor {
 
-    private HttpClient httpClient;
-    private ExternalPaymentLoadBalancer loadBalancer;
-    private Map<String, AtomicLong> errorCount = new ConcurrentHashMap<>();
+    private final HttpClient httpClient;
+    private final ExternalPaymentLoadBalancer loadBalancer;
 
-    /**
-     * Required by CDI, Don't use it directly.
-     */
-    @Deprecated
-    public ExternalPaymentProcessor() {
-    }
-
-    @Inject
     public ExternalPaymentProcessor(
             HttpClient httpClient,
             ExternalPaymentLoadBalancer loadBalancer) {
@@ -44,32 +31,11 @@ public class ExternalPaymentProcessor {
             var request = createRequest(data.url().resolve("/payments"), payment);
             var response = httpClient.send(request, HttpResponse.BodyHandlers.discarding());
             if (response.statusCode() == 200) {
-                try {
                     return Optional.of(payment);
-                } finally {
-                    errorCount.remove(payment.correlationId());
-                }
             }
-            errorCount.computeIfAbsent(payment.correlationId(), k -> new AtomicLong(0)).incrementAndGet();
             return Optional.empty();
         } catch (Exception e) {
             return Optional.empty();
-        } finally {
-            sleepIfNeed(payment);
-        }
-    }
-
-    private void sleepIfNeed(ProcessedPayment payment) {
-        long errorNumber = errorCount
-                .computeIfAbsent(payment.correlationId(), k -> new AtomicLong(0))
-                .get();
-        long delay = errorNumber * 100;
-        if (delay > 0) {
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                // Restore the interrupted status
-            }
         }
     }
 
