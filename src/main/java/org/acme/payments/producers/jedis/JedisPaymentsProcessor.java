edbfe3d6673d3849aa2ff60eb93cb17a6be76e8c
Maximillian Arruda <dearrudam@gmail.com>
2025-08-31 00:09:45 -0300

    fix: ajustar controle de execução do processador de pagamentos
    Modifica a lógica de controle de execução do processador de pagamentos para garantir que ele possa ser iniciado e interrompido corretamente, evitando threads em execução desnecessárias.

--- src/main/java/org/acme/payments/producers/jedis/JedisPaymentsProcessor.java
+++ src/main/java/org/acme/payments/producers/jedis/JedisPaymentsProcessor.java
@@ -1,36 +1,58 @@
 package org.acme.payments.producers.jedis;
 
 import jakarta.json.bind.Jsonb;
+import org.acme.payments.domain.ExternalPaymentProcessor;
 import org.acme.payments.domain.PaymentRequest;
 import org.acme.payments.domain.PaymentsProcessor;
 import org.acme.payments.domain.PaymentsRepository;
 import org.acme.payments.domain.ProcessedPayment;
-import org.acme.payments.domain.ExternalPaymentProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.UnifiedJedis;
 import redis.clients.jedis.exceptions.JedisException;
 
-import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public record JedisPaymentsProcessor(
-        Jsonb jsonb,
-        UnifiedJedis jedis,
-        PaymentsRepository paymentsRepository,
-        ExternalPaymentProcessor externalPaymentProcessor,
-        LinkedBlockingQueue<PaymentRequest> queue,
-        int workersSize,
-        ExecutorService executeService
+public class JedisPaymentsProcessor implements PaymentsProcessor {
 
-) implements PaymentsProcessor {
+    private final static Logger logger = LoggerFactory.getLogger(JedisPaymentsProcessor.class);
 
     public static final String PAYMENTS_QUEUE = "payments-queued";
 
-    public JedisPaymentsProcessor start() {
+    private volatile boolean running = false;
+    private final Jsonb jsonb;
+    private final UnifiedJedis jedis;
+    private final PaymentsRepository paymentsRepository;
+    private final ExternalPaymentProcessor externalPaymentProcessor;
+    private final LinkedBlockingQueue<PaymentRequest> queue;
+    private final int workersSize;
+    private final ExecutorService executeService;
 
+    public JedisPaymentsProcessor(Jsonb jsonb,
+                                  UnifiedJedis jedis,
+                                  PaymentsRepository paymentsRepository,
+                                  ExternalPaymentProcessor externalPaymentProcessor,
+                                  LinkedBlockingQueue<PaymentRequest> queue,
+                                  int workersSize,
+                                  ExecutorService executeService) {
+        this.jsonb = jsonb;
+        this.jedis = jedis;
+        this.paymentsRepository = paymentsRepository;
+        this.externalPaymentProcessor = externalPaymentProcessor;
+        this.queue = queue;
+        this.workersSize = workersSize;
+        this.executeService = executeService;
+    }
+
+    public JedisPaymentsProcessor start() {
+        if (running) {
+            return this;
+        }
+        running = true;
         // Initialize the queue in Redis
         int initiatedWorker = 0;
         do {
@@ -42,7 +64,7 @@ public record JedisPaymentsProcessor(
         // Start a separate thread to handle queuing payment requests to Redis
         // This thread will take payment requests from the queue and push them to Redis
         this.executeService.execute(() -> {
-            while (true) {
+            while (running) {
                 try {
                     PaymentRequest paymentRequest = queue.take();
                     queueInRedis(paymentRequest);
@@ -56,37 +78,21 @@ public record JedisPaymentsProcessor(
     }
 
     public void shutdown() {
-        if (executeService != null) {
-            executeService.shutdown();
-            int maxWaitSeconds = 5;
-            if (!executeService.isTerminated()) {
-                try {
-                    Thread.sleep(Duration.ofSeconds(maxWaitSeconds).toMillis());
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt(); // Restore interrupted status
-                }
-                if (!executeService.isTerminated()) {
-                    executeService.shutdownNow();
-                }
-            }
-        }
+        this.running = false;
     }
 
     private Runnable getPaymentTask() {
         return () -> this.listenForPayments(
                 this::retrievePaymentRequest,
                 paymentsRepository::save,
-                paymentRequest -> {
-                    // On processing failure, re-queue the payment request
-                    this.queueInRedis(paymentRequest);
-                }
+                this::queueInRedis // On processing failure, re-queue the payment request
         );
     }
 
     public void listenForPayments(Supplier<Optional<PaymentRequest>> paymentRequestSupplier,
                                   Consumer<ProcessedPayment> processedPaymentConsumer,
                                   Consumer<PaymentRequest> onProcessingFailed) {
-        while (!executeService.isShutdown()) {
+        while (running) {
             try {
                 Optional<PaymentRequest> receivedPaymentRequest = paymentRequestSupplier.get();
                 // Process the payment request if it was received
@@ -96,10 +102,10 @@ public record JedisPaymentsProcessor(
                                         processedPaymentConsumer,
                                         () -> onProcessingFailed.accept(paymentRequest)
                                 ));
-            } catch (RuntimeException e) {
-                if (e instanceof JedisException) {
+            } catch (RuntimeException ex) {
+                if (ex instanceof JedisException jedisEx) {
                     // printing any Jedis exception stack trace
-                    e.printStackTrace();
+                    logger.warn("Jedis exception occurred: {}", jedisEx.getMessage(), jedisEx);
                 }
             }
         }
